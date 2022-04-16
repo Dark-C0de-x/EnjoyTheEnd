445e38fa63009464a846cb36bae612131833340b
Dark_C0de <64330628+Dark-C0de-x@users.noreply.github.com>
2022-04-16 23:00:17 +0200

    Update sec.py

--- sec.py
+++ sec.py
@@ -1,19 +1,15 @@
-from ast import While
 import requests
-
-
-response = requests.get("https://ipgeolocation.abstractapi.com/v1/?api_key=61e6f52fa0424f5aa17d435f93b63637&fields=security")
+import os
 
 not_connected = b'{"security":{"is_vpn":false}}'
+connected = b'{"security":{"is_vpn":true}}'
 
-def new_func(response, not_connected):
-    While response.content = not_connected:
-
-
-if response.content == not_connected:
-    print("[-] No conectado a tor")
-elif response.status_code != 200:
-    print("[-] Ha habido un error con la petición")
-else:
-    print("[+] Conectado correctamente!")
-new_func(response, not_connected)
+while True:
+    response = requests.get("https://ipgeolocation.abstractapi.com/v1/?api_key=61e6f52fa0424f5aa17d435f93b63637&fields=security")
+    if response.content == not_connected:
+        print("[-] No conectado a tor")
+    elif response.status_code != 200:
+        print("[-] Ha habido un error con la petición")
+    else:
+        print("[+] Conectado correctamente!")
+        os.system("sleep 5")
