52174bffb77d1904ae475ab703c3b3fe8cfa1c0d
Dark_C0de <64330628+Dark-C0de-x@users.noreply.github.com>
2022-04-17 14:27:33 +0200

    notificación de la desconexión de tor.

--- sec.py
+++ sec.py
@@ -1,17 +1,24 @@
 import requests
 import os
+import platform
+from plyer import notification
 
 not_connected = b'{"security":{"is_vpn":false}}'
 connected = b'{"security":{"is_vpn":true}}'
 
+def notify(message, title):
+    if platform.system() == 'Darwin':
+        os.system("osascript -e 'display notification \"{}\" with title \"{}\"'".format(message, title))
+    else:
+        notification.notify(
+            title=title,
+            message=message)
 while True:
     response = requests.get("https://ipgeolocation.abstractapi.com/v1/?api_key=61e6f52fa0424f5aa17d435f93b63637&fields=security")
     if response.content == not_connected:
-        print("[-] No conectado a tor")
+        notify('se tensó que flipas manin', 'TOR DESCONECTADO')
     elif response.status_code != 200:
         print("[-] Ha habido un error con la petición")
     else:
         print("[+] Conectado correctamente!")
         os.system("sleep 5")
-        
-        
