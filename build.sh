f39eebdf0a16e765b953d2ecf7d4b68780978ab9
Dark_C0de <64330628+Dark-C0de-x@users.noreply.github.com>
2022-04-18 19:39:34 +0200

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,1 +1,18 @@
 echo ""
+user=$(whoami)
+if [ "$user" == "root" ]; then
+   esroot=true
+   echo "[+] Comenzando instalacion"
+   echo ""
+else
+   esroot=false
+   echo "[*] Se necesita root" 
+   exit
+fi
+apt-get install python3
+pip3 install plyer
+pip3 install requests
+pip3 install platform
+apt-get install go -y
+echo ""
+echo "Instalacion completada satisfactoriamente, puede ejecutar gh0st.py"
