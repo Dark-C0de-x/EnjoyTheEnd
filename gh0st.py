9ad8a85a30d564c14c3c38876320382f14f702b7
Dark_C0de <64330628+Dark-C0de-x@users.noreply.github.com>
2022-06-18 13:07:16 -0400

    Add files via upload

---
+++ gh0st.py
@@ -0,0 +1,149 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+#if os.geteuid() != 0:
+#    print('[-] Necesitas ser root')
+#    sys.exit(1)
+#else:
+#    os.system ("clear")
+
+def ShinigamiEye():
+	os.system("cls")
+	print("	⢴⣶⣿⣿⣷⣾⣷⣶⣴⣄⢰⣶⣷⡄⠀⠀⠀⡀⠀⢀⣠⣴⣴⣶⡶⠷⠒⠂")
+	print("⠀⠁⠛⡿⣿⣿⣿⣿⣿⣿⣷⣼⣿⣿⣃⣠⣴⣿⣾⣿⣿⣿⣿⣿⠟⠀⠀⠀⠀")
+	print("⠀⠀⠀⠀⠋⠉⢿⠋⢿⣿⢿⣿⣿⣿⣿⣿⣿⡿⣿⡿⠿⠛⠏⠉⠀⠀⠀⠀⠀")
+	print("⠀⠀⠀⠀⠀⠀⠀⠀⢸⠇⠀⢻⣿⣿⠋⠸⣿⠀⠘⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+	print("⠀⠀⠀⠀⠀⠀⠀⠀⣸⠀⢠⣿⣿⣿⡄⠀⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+	print("⠀⠀⠀⠀⠀⠀⠀⠀⠸⣇⣾⣿⣿⣿⣷⣤⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+	print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⡟⠉⠈⠈⠙⣿⣇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+	print("⠀⠀⠀⠀⠀⠀⠀⠀⠸⣿⣿⡢⠀⠀⢾⣿⡿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+	print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⢻⡟⠓⠀⠀⠚⡿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+	print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣇⠀⠀⠀⣸⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+	print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⡟⠀⠀⠀⠘⢧⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+	print("⠀⠀⠀⠀⠀⠀⠀⠀⠀⠘⠁⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀")
+	print("")
+	
+	print("Busca Tor, I2P, fugas de datos y bases de datos filtradas por correo electrónico, dominio, IP, CIDR, dirección de Bitcoin y más")
+	print("")
+	string = input("Inserta palabra calave -> ")
+	os.system("python osint.py -search " + string)
+def DoS():
+	os.system("cls")
+	print("   .... NO! ...                  ... MNO! ...")
+	os.system("sleep 0.1")
+	print("   ..... MNO!! ...................... MNNOO! ...")
+	os.system("sleep 0.1")
+	print(" ..... MMNO! ......................... MNNOO!! .")
+	os.system("sleep 0.1")
+	print("..... MNOONNOO!   MMMMMMMMMMPPPOII!   MNNO!!!! .")
+	os.system("sleep 0.1")
+	print("... !O! NNO! MMMMMMMMMMMMMPPPOOOII!! NO! ....")
+	os.system("sleep 0.1")
+	print("   ...... ! MMMMMMMMMMMMMPPPPOOOOIII! ! ...")
+	os.system("sleep 0.1")
+	print("   ........ MMMMMMMMMMMMPPPPPOOOOOOII!! .....")
+	os.system("sleep 0.1")
+	print("  ........ MMMMMOOOOOOPPPPPPPPOOOOMII! ...")
+	os.system("sleep 0.1")
+	print("   ....... MMMMM..    OPPMMP    .,OMI! ....")
+	os.system("sleep 0.1")
+	print("    ...... MMMM::   o.,OPMP,.o   ::I!! ...")
+	os.system("sleep 0.1")
+	print("        .... NNM:::.,,OOPM!P,.::::!! ....")
+	os.system("sleep 0.1")
+	print("         .. MMNNNNNOOOOPMO!!IIPPO!!O! .....")
+	os.system("sleep 0.1")
+	print("        ... MMMMMNNNNOO:!!:!!IPPPPOO! ....")
+	os.system("sleep 0.1")
+	print("          .. MMMMMNNOOMMNNIIIPPPOO!! ......")
+	os.system("sleep 0.1")
+	print("         ...... MMMONNMMNNNIIIOO!..........")
+	os.system("sleep 0.1")
+	print("      ....... MN MOMMMNNNIIIIIO! OO ..........")
+	os.system("sleep 0.1")
+	print("   ......... MNO! IiiiiiiiiiiiI OOOO ...........")
+	os.system("sleep 0.1")
+	print("  ...... NNN.MNO! . O!!!!!!!!!O . OONO NO! ........")
+	os.system("sleep 0.1")
+	print("   .... MNNNNNO! ...OOOOOOOOOOO .  MMNNON!........")
+	os.system("sleep 0.1")
+	print("  ...... MNNNNO! .. PPPPPPPPP .. MMNON!........")
+	os.system("sleep 0.1")
+	print("     ...... OO! ................. ON! .......")
+	os.system("sleep 0.1")
+	print("       ................................")
+	print("")
+	print("(Ejemplo: https://www.google.es)")
+	print("")
+	url = input("Ingresa url objetivo -> ")
+	print("[*] iniciando ataque...")
+	print("")
+	os.system("go run hulk.go -site " + url)
+
+
+
+
+print("                          ▓▓████████████████                    ")
+print("                    ▒▒██▓▓              ▒▒██████░░              ")
+print("                ▓▓██░░    ▓▓██████████████    ██████            ")
+print("            ██▒▒  ░░██████████████████████████    ██████        ")
+print("          ██  ▒▒██░░  ████████████████████░░▒▒██▒▒  ▓▓████      ")
+print("        ██  ██        ████████    ██████████    ████  ▒▒████░░  ")
+print("      ▒▒            ████████        ████████      ████    ██████")
+print("    ░░░░            ██    ██        ████████        ████  ░░████")
+print("    ░░              ██    ██        ████████          ████    ██")
+print("                      ▒▒████████████████████          ██████    ")
+print("                      ████████████████████          ░░██████    ")
+print("      ░░                ████████████████          ░░██▒▒████    ")
+print("          ░░              ▒▒████████▒▒          ████            ")
+print("            ▒▒▒▒░░                          ▒▒██                ")
+print("                  ░░▒▒██                ▓▓██                    ")
+print("")
+print("")
+print("")
+
+
+def menu():
+	print ("")
+	print ("                   -Let's end with them-")
+	print ("")
+	print ("                    [1] ==> Destroy")
+	print ("                    [2] ==> Anonymize")
+	print ("                    [3] ==> Tor Verify")
+	print ("                    [4] ==> ShinigamiEye")
+	print ("                    [5] ==> salir")
+ 
+ 
+while True:
+	# Mostramos el menu
+	menu()
+ 
+	# solicituamos una opción al usuario
+	opcionMenu = input("#> ")
+ 
+	if opcionMenu=="1":
+		print ("")
+		DoS()
+		os.system ("cls")
+	elif opcionMenu=="2":
+		print ("")
+		os.system("macchanger -r eth0")
+		os.system ("cls")
+	elif opcionMenu=="3":
+		print ("")
+		os.system("gnome-terminal --command=python sec.py")
+		os.system ("cls")
+	elif opcionMenu=="4":
+		print ("")
+		os.system ("cls")
+		ShinigamiEye()
+	elif opcionMenu=="5":
+		print("Por el culo te la hinco tontito xd")
+		break
+	else:
+		print ("")
+		input("No has pulsado ninguna opción correcta...\npulsa una tecla para continuar")
+
+
